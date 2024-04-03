d63ad114f95f5ace6d91305ff9e150ef9cfe46f7
Emon-Khan <rkemon94@gmail.com>
2024-04-03 19:46:01 +0600

    Functionality of Rating added successfully

--- controller/rating_controller.py
+++ controller/rating_controller.py
@@ -1,4 +1,15 @@
 from app import app
-@app.route('/movie/rating/add')
-def ratingAdd():
-    return "This is a page for adding rating for a specific movie"
+from model.AuthModel import AuthModel
+from model.RatingModel import RatingModel
+from flask import request
+rating_obj = RatingModel()
+auth = AuthModel()
+
+@app.route('/rating/add', methods=['POST'])
+@auth.token_auth()
+def rating_add_controller():
+    return rating_obj.rating_add_model(request.json)
+
+@app.route('/rating/getall')
+def rating_getall_controller():
+    return rating_obj.rating_getall_model()
